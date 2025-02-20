d2041ea432fab8c3757ac8ed7052ef033892137c
joel <email>
2025-02-20 22:38:58 +0100

    add jwt class

--- GigFinder/Controllers/AuthController.cs
+++ GigFinder/Controllers/AuthController.cs
@@ -1,29 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web.Helpers;
 using System.Web.Http;
-using System.Web.Http.Description;
 using GigFinder.Models;
-using BCrypt.Net;
 using GigFinder.Controllers.Request;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-
+using GigFinder.Utils;
 namespace GigFinder.Controllers
 {
     public static class AuthMessages
     {
         public const string EXISTS = "exists";
-
+        public const string INVALID_EMAIL_OR_PASSWORD = "invalid_email_or_password";
     }
 
     public static class UserTypes
@@ -69,28 +58,6 @@ namespace GigFinder.Controllers
             return newUser;
         }
 
-        private string generateUserJwt(User user)
-        {
-            var key = "61e6ac9f0e7ac2c9648805ccb06728486f663180ae0375a633d810421d5adf0e5126cfb4dc78a5997e953df14dd74751cd0bc0111a5a7ba1b8757431fb7780a634243787e5fac11f7c599af56c15080d93e28b76d6a86851a8966e5228ca6115d8b948d796410c6dd7d56fe1ce0dd086fc38a31a953caedfe5b9882e7d9168f488036677cc31d9b7d7fd462726e80ad55238da2223b9c84450733cfa4e4438583f243a0bd954f2ab8b2b166a6e79c620d7e6e61e2b379a6a621cad45cff81af3bb3ad1e668376240a05767a2f23f4d5a0f73aa0d69f511dab3cd0188b3ea1230c713d4e542887a7f34507377f27bc77ddef44188aa0169a1e81a671d86cc5792";
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)); // Replace with a secure key
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-            var claims = new[]
-            {
-                 new Claim("user_id", user.id.ToString())
-            };
-
-            var token = new JwtSecurityToken(
-                issuer: "gigfinder",
-                audience: "gigfinder",
-                claims: claims,
-                expires: DateTime.UtcNow.AddYears(1),  // Token expiry time set to 1 years
-                signingCredentials: credentials
-            );
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
-
         // POSt: api/auth/signup/musician
         [HttpPost]
         [Route("signup/musician")]
@@ -104,7 +71,6 @@ namespace GigFinder.Controllers
                     return BadRequest(ModelState);
                 }
                 // validate user does not exists
-                IHttpActionResult res;
                 User user = await db.Users
                                 .Where(u => u.email == request.Email)
                                 .FirstOrDefaultAsync();
@@ -135,7 +101,7 @@ namespace GigFinder.Controllers
 
                 // create musican
 
-                return Ok(generateUserJwt(newUser));
+                return Ok(Jwt.GenerateUserJwt(newUser));
             }
             catch (Exception e)
             {
@@ -156,7 +122,6 @@ namespace GigFinder.Controllers
                     return BadRequest(ModelState);
                 }
                 // validate user does not exists
-                IHttpActionResult res;
                 User user = await db.Users
                                 .Where(u => u.email == request.Email)
                                 .FirstOrDefaultAsync();
@@ -187,7 +152,42 @@ namespace GigFinder.Controllers
 
                 // create musican
 
-                return Ok(generateUserJwt(newUser));
+                return Ok(Jwt.GenerateUserJwt(newUser));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IHttpActionResult> Login([FromBody] RequestLogin request)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    // Return BadRequest with validation errors
+                    return BadRequest(ModelState);
+                }
+                // validate user does  exists
+                User user = await db.Users
+                                .Where(u => u.email == request.Email)
+                                .FirstOrDefaultAsync();
+                if (user == null)
+                {
+                    return BadRequest(AuthMessages.INVALID_EMAIL_OR_PASSWORD);
+                }
+
+                // validate user password
+                var correctPassword = BCrypt.Net.BCrypt.Verify(request.Password, user.password);
+                if (!correctPassword)
+                {
+                    return BadRequest(AuthMessages.INVALID_EMAIL_OR_PASSWORD);
+                }
+
+                return Ok(Jwt.GenerateUserJwt(user));
             }
             catch (Exception e)
             {
---
+++ GigFinder/Controllers/Request/RequestLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GigFinder.Controllers.Request
+{
+    public class RequestLogin
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        [MaxLength(100, ErrorMessage = "Email must be maxium 2 character.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(1, ErrorMessage = "Password must be at least 1 character.")]
+        public string Password { get; set; }
+    }
+}
