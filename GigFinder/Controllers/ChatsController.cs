55015f7ceaee6fce9515ad7560219bfa174d05c7
joel <email>
2025-02-22 03:03:50 +0100

    create chatroom

---
+++ GigFinder/Controllers/ChatsController.cs
@@ -0,0 +1,96 @@
+﻿using GigFinder.Attributes;
+using GigFinder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using GigFinder.Controllers.Request;
+using GigFinder.Utils;
+using System.Data.Entity;
+
+namespace GigFinder.Controllers
+{
+    [RoutePrefix("api/chats")]
+    [ProtectedUser]
+    public class ChatsController: ApiController
+    {
+
+        private gigfinderEntities1 db = new gigfinderEntities1();
+
+        private async Task<ChatRoom> GetChatRoom(int user1, int user2)
+        {
+            if(user1 == user2)
+            {
+                throw new Exception("you cant open a chat room with yourself");
+            }
+            return await db.ChatRooms
+               .Where(c => (c.user_id1 == user1 && c.user_id2 == user2)
+                        || (c.user_id1 == user2 && c.user_id2 == user1))
+               .FirstOrDefaultAsync();
+        }
+
+        [HttpGet]
+        [Route("{otherUser}/getchat")]
+        public async Task<IHttpActionResult> GetUserChatRoom(int otherUser)
+        {
+            IHttpActionResult response;
+            try
+            {
+
+                User user = UserUtils.GetCurrentUser();
+
+                ChatRoom chatRoom = await GetChatRoom(user.id, otherUser);
+    
+                if (chatRoom == null)
+                {
+                    int firstUser = user.id < otherUser ? user.id : otherUser;
+                    int secondUser = user.id > otherUser ? user.id : otherUser;
+                    ChatRoom newChat = new ChatRoom
+                    {
+                        user_id1 = firstUser,
+                        user_id2 = secondUser,
+                        
+                    };
+                    db.ChatRooms.Add(newChat);
+                    await db.SaveChangesAsync();
+                    response = Ok(newChat.id);
+                }
+                else
+                {
+                    response = Ok(chatRoom.id);
+                }
+            }catch(Exception e)
+            {
+                response = BadRequest(e.ToString());
+            }
+                
+            return response;
+        }
+
+        [HttpPost]
+        [Route("{chatId}/send-message")]
+        public async Task<IHttpActionResult> SendMessage(int chatId, [FromBody] RequestSendMessage request)
+        {
+            return Ok();
+
+        }
+
+        [HttpGet]
+        [Route("{chatId}/messages")]
+        public async Task<IHttpActionResult> GetChatMessages(int chatId)
+        {
+            return Ok();
+
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public async Task<IHttpActionResult> GetAllChats()
+        {
+            return Ok();
+
+        }
+    }
+}
---
+++ GigFinder/Controllers/Request/RequestSendMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GigFinder.Controllers.Request
+{
+    public class RequestSendMessage
+    {
+        [Required(ErrorMessage = "Content is required.")]
+        [MinLength(1, ErrorMessage = "Content must be at least 1 character.")]
+        [MaxLength(255, ErrorMessage = "Content must be maxium 255 character.")]
+
+        public string Content { get; set; }
+
+        [Required(ErrorMessage = "Start date is required.")]
+        public DateTime Date { get; set; }
+    }
+}
